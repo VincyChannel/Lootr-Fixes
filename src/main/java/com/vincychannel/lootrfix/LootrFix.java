6f31e99f44079cbafc5bac6c5ca8ff86e884600a
Vincenzo Roberti <robertienzo190@gmail.com>
2025-07-12 04:05:20 +0200

    Added Update URL for Forge Version Checker

--- src/main/java/com/vincychannel/lootrfix/LootrFix.java
+++ src/main/java/com/vincychannel/lootrfix/LootrFix.java
@@ -18,7 +18,7 @@ public class LootrFix {
     public static final String MODID = "lootrfix";
     public static final String NAME = "Lootr Fixes";
     public static final String VERSION = "1.0";
-    public static final String UPDATE_URL = "https://raw.githubusercontent.com/VincyChannel/Lootr-Fixes/refs/heads/test/update.json";
+    public static final String UPDATE_URL = "https://raw.githubusercontent.com/VincyChannel/Lootr-Fixes/refs/heads/main/update.json";
 
     public static final Logger LOGGER = LogManager.getLogger(MODID);
 
