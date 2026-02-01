12fe7bdbe21e24163331a16391c2c39475e7d686
Vincenzo Roberti <robertienzo190@gmail.com>
2026-02-01 19:42:47 +0100

    Update to Version 1.2

--- src/main/java/com/vincychannel/lootrfix/LootrFix.java
+++ src/main/java/com/vincychannel/lootrfix/LootrFix.java
@@ -17,7 +17,7 @@ import org.apache.logging.log4j.Logger;
 public class LootrFix {
     public static final String MODID = "lootrfix";
     public static final String NAME = "Lootr Fixes";
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "1.2";
     public static final String UPDATE_URL = "https://raw.githubusercontent.com/VincyChannel/Lootr-Fixes/refs/heads/main/update.json";
 
     public static final Logger LOGGER = LogManager.getLogger(MODID);
